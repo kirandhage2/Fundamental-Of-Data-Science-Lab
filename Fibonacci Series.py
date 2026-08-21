89bd057a030b0b1b646453e86e96d8d92a51f97a
Kiran Dhage <dhagekiran5556@gmail.com>
2026-08-21 16:01:38 +0530

    Delete Fibonacci Series.py

--- Fibonacci Series.py
+++
@@ -1,9 +0,0 @@
-# Fibonacci Series up to n terms
-
-n = int(input("Enter the number of terms: "))
-
-a, b = 0, 1
-print("Fibonacci Series:")
-for i in range(n):
-    print(a, end=" ")
-    a, b = b, a + b
